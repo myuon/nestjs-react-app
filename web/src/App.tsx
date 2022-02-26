ac100f1bfbe0ba34657d31219ed62bcfcdf5a2fb
myuon <ioi.joi.koi.loi@gmail.com>
2022-02-26 22:46:53 +0900

    feat: request to API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface Me {
+  name: string;
+  now: number;
+}
+
+const useMe = () => {
+  const [state, setState] = useState<Me>();
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch('http://localhost:3000/api/me');
+      if (resp.ok) {
+        setState(await resp.json());
+      } else {
+        console.error(await resp.text());
+      }
+    })();
+  }, []);
+
+  return state;
+};
+
 function App() {
   const [count, setCount] = useState(0);
+  const me = useMe();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>
+          Hello {me?.name}, it is {me?.now && new Date(me.now).toISOString()}{' '}
+          now.
+        </p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
