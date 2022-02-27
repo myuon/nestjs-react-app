c1318ac19df90c4f1d6d4f1f3e03e646168b73a1
myuon <ioi.joi.koi.loi@gmail.com>
2022-02-27 19:11:13 +0900

    chore: SSR option to disable

--- web/server.js
+++ web/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const express = require('express');
 const { createServer: createViteServer } = require('vite');
 
+const SSR = process.env.SSR || true;
+
 async function createServer() {
   const app = express();
 
@@ -25,32 +27,22 @@ async function createServer() {
     const url = req.originalUrl;
 
     try {
-      // 1. Read index.html
       let template = fs.readFileSync(
-        path.resolve(__dirname, 'index.ssr.html'),
+        path.resolve(__dirname, SSR ? 'index.ssr.html' : 'index.html'),
         'utf-8',
       );
 
-      // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
-      //    also applies HTML transforms from Vite plugins, e.g. global preambles
-      //    from @vitejs/plugin-react
       template = await vite.transformIndexHtml(url, template);
 
-      // 3. Load the server entry. vite.ssrLoadModule automatically transforms
-      //    your ESM source code to be usable in Node.js! There is no bundling
-      //    required, and provides efficient invalidation similar to HMR.
-      const { render } = await vite.ssrLoadModule('./src/entry-server.tsx');
+      if (SSR) {
+        const { render } = await vite.ssrLoadModule('./src/entry-server.tsx');
+        const appHtml = await render(url);
+        const html = template.replace(`<!--ssr-outlet-->`, appHtml);
 
-      // 4. render the app HTML. This assumes entry-server.js's exported `render`
-      //    function calls appropriate framework SSR APIs,
-      //    e.g. ReactDOMServer.renderToString()
-      const appHtml = await render(url);
-
-      // 5. Inject the app-rendered HTML into the template.
-      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
-
-      // 6. Send the rendered HTML back.
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+        res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+      } else {
+        res.status(200).end(template);
+      }
     } catch (e) {
       // If an error is caught, let Vite fix the stracktrace so it maps back to
       // your actual source code.
@@ -59,7 +51,7 @@ async function createServer() {
     }
   });
 
-  app.listen(3000);
+  app.listen(3030);
 }
 
 createServer();
